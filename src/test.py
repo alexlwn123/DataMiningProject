9ee51756a8ebb0090d111bb33684d71f76c7901c
Alex Lewin <asl0028@auburn.edu>
2020-11-08 19:01:03 -0600

    updates

--- src/test.py
+++ src/test.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from scipy.signal import convolve2d
 def main():
-  data = pd.read_csv("data/data.csv")
-
+  data = pd.read_csv("data\CA-GrQc.txt", sep="\t")
+   # com-dblp.ungraph.txt
+  print(data)
 
 
 if __name__ == '__main__':
